1af6dfd56331bf1e884df7f5fca5b0ba3313a8e1
seanathon81 <90300066+seanathon81@users.noreply.github.com>
2021-10-18 11:14:09 -0700

    shop functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -95,6 +95,15 @@ for (var i = 0; i < enemyNames.length; i++) {
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
   }
+
+    if (playerHealth > 0 && i < enemyNames.length -1) {
+    var storeConfirm = window. confirm("The fight is over, visit the store before the next round?");
+   // if yes, take them to the store() function
+   if (storeConfirm) {
+     shop()
+  }
+}
+  
   // if player isn't alive, stop the game
   else {
     window.alert('You have lost your robot in battle! Game Over!');
@@ -127,6 +136,52 @@ if (playAgainConfirm) {
 else{
 window.alert("Thank you for playing Robort Gladiators! Come back soon!")
 }
+};
+
+var shop = function() {
+  var shopOptionPrompt = window.prompt (
+  );
+// use switch to carry out action
+switch (shopOptionPrompt) {
+  case "REFILL":  // new case
+  case "refill":
+    if (playerMoney >= 7) {
+    window.alert("Refilling player's health by 20 for 7 dollars.");
+
+    // increase health and decrease money
+    playerHealth = playerHealth + 20;
+    playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+  case "UPGRADE": // new case
+  case "upgrade":
+    if (playerMoney >= 7) {
+    window.alert("Upgrading player's attack by 6 for 7 dollars."); 
+
+    // increase attack and decrease money
+    playerAttack = playerAttack + 6;
+    playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!")
+    }
+    break;
+  case "LEAVE":
+  case "leave":
+    window.alert("Leaving the store.");
+
+    // do nothing, so function will end
+    break;
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+
+    // call shop() again to force player to pick a valid option
+    shop();
+    break;
+}
+
 };
 
 // start the game when the page loads
